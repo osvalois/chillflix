e53ec7f1fecbb7e6766dbfe2f162dfb294ca27e2
Oscar Valois <118651508+osvalois@users.noreply.github.com>
2024-12-28 07:33:20 -0600

    Update SiteSettings.tsx

--- src/pages/SiteSettings.tsx
+++ src/pages/SiteSettings.tsx
@@ -194,6 +194,7 @@ const StatCard: React.FC<{
 
   return (
     <MotionBox
+      p={6}
       borderRadius="2xl"
       bg={bgColor}
       backdropFilter="blur(20px)"
@@ -403,6 +404,7 @@ const SiteSettings: React.FC = () => {
       <Container maxW="7xl" pt="100px">
         <Parallax translateY={[-20, 20]}>
           <MotionFlex
+            direction="column"
             w="full"
             borderRadius="3xl"
             overflow="hidden"
@@ -545,6 +547,7 @@ const SiteSettings: React.FC = () => {
                 <MotionBox
                   whileHover={{ scale: 1.05 }}
                   transition={{ duration: 0.2 }}
+                  position="relative"
                 >
                   <Avatar
                     size="2xl"
